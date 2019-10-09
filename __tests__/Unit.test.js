a1d42f778cd14d0e9976be799774005a03e36823
Aleksandr Serov <sashaserov2010@gmail.com>
2019-10-09 12:26:42 +0300

    update unt test

--- __tests__/Unit.test.js
+++ __tests__/Unit.test.js
@@ -18,7 +18,7 @@ describe('Testings helpers', () => {
 
   test('convertInitialState empty', () => {
     // Empty state
-    const emptyState = {
+    const expected = {
       messages: {
         allIds: [],
         byId: {},
@@ -32,7 +32,7 @@ describe('Testings helpers', () => {
       },
       currentChannelId: 1,
     };
-    const state = {
+    const initialState = {
       messages: [],
       channels: [
         { id: 1, name: 'general', removable: false },
@@ -40,8 +40,43 @@ describe('Testings helpers', () => {
       ],
       currentChannelId: 1,
     };
-    const expected = emptyState;
-    expect(convertInitialState(state)).toEqual(expected);
+    expect(convertInitialState(initialState)).toEqual(expected);
   });
-  // test('convertInitialState not empty', () => {});
+
+  test('convertInitialState not empty', () => {
+    const expected = {
+      messages: {
+        allIds: [4],
+        byId: {
+          '4': {
+            text: 'test',
+            username: 'Mrs. Alberto Huels',
+            channelId: 1,
+            id: 4,
+          },
+        },
+      },
+      channels: {
+        allIds: [1, 2, 3],
+        byId: {
+          '1': { id: 1, name: 'general', removable: false },
+          '2': { id: 2, name: 'random', removable: false },
+          '3': { id: 3, name: 'test', removable: true },
+        },
+      },
+      currentChannelId: 1,
+    };
+    const initialState = {
+      channels: [
+        { id: 1, name: 'general', removable: false },
+        { id: 2, name: 'random', removable: false },
+        { id: 3, name: 'test', removable: true },
+      ],
+      messages: [
+        { text: 'test', username: 'Mrs. Alberto Huels', channelId: 1, id: 4 },
+      ],
+      currentChannelId: 1,
+    };
+    expect(convertInitialState(initialState)).toEqual(expected);
+  });
 });
