aca9e73cf448aaf7afaeb79c3ac336fe6e11f19f
Aleksandr Serov <sashaserov2010@gmail.com>
2019-10-29 07:12:29 +0300

    make module

--- src/entry.jsx
+++ src/entry.jsx
@@ -18,54 +18,55 @@ import actionCreators from './actions';
 import App from './components/App';
 import UsernameContext from './usernameContext';
 import './i18n';
-/* eslint-disable no-underscore-dangle */
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-  : compose;
-/* eslint-enable */
-
-const initialState = convertInitialState(gon);
-const username = Cookies.get('username') || faker.name.findName();
-if (!Cookies.get('username')) {
-  Cookies.set('username', username);
-}
-
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeEnhancers(applyMiddleware(thunk)),
-);
-
-const socket = io();
-const { dispatch, getState } = store;
-const actions = bindActionCreators(actionCreators, dispatch);
-
-socket.on('newMessage', ({ data }) => {
-  const { attributes: message } = data;
-  actions.addMessage({ message });
-});
-socket.on('newChannel', ({ data }) => {
-  const { attributes: channel } = data;
-  actions.addChannel({ channel });
-});
-socket.on('removeChannel', ({ data }) => {
-  const { id } = data;
-  const { channels, messages, currentChannelId } = getState();
-  actions.removeChannelFromStore({ id, messages });
-  if (currentChannelId !== id) {
-    return;
-  }
-  const [firstChannelId] = channels.allIds;
-  actions.setCurrentChannelId({
-    id: firstChannelId,
-  });
-});
-socket.on('renameChannel', ({ data }) => {
-  const { attributes: channel } = data;
-  actions.updateChannel({ channel });
-});
 
 export default () => {
+  /* eslint-disable no-underscore-dangle */
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
+  /* eslint-enable */
+
+  const initialState = convertInitialState(gon);
+  const username = Cookies.get('username') || faker.name.findName();
+  if (!Cookies.get('username')) {
+    Cookies.set('username', username);
+  }
+
+  const store = createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(thunk)),
+  );
+
+  const socket = io();
+  const { dispatch, getState } = store;
+  const actions = bindActionCreators(actionCreators, dispatch);
+
+  socket.on('newMessage', ({ data }) => {
+    const { attributes: message } = data;
+    actions.addMessage({ message });
+  });
+  socket.on('newChannel', ({ data }) => {
+    const { attributes: channel } = data;
+    actions.addChannel({ channel });
+  });
+  socket.on('removeChannel', ({ data }) => {
+    const { id } = data;
+    const { channels, messages, currentChannelId } = getState();
+    actions.removeChannelFromStore({ id, messages });
+    if (currentChannelId !== id) {
+      return;
+    }
+    const [firstChannelId] = channels.allIds;
+    actions.setCurrentChannelId({
+      id: firstChannelId,
+    });
+  });
+  socket.on('renameChannel', ({ data }) => {
+    const { attributes: channel } = data;
+    actions.updateChannel({ channel });
+  });
+
   ReactDOM.render(
     <Provider store={store}>
       <UsernameContext.Provider value={{ username }}>
